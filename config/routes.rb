3b24bc4e5ac81fb3bc1a84fea3afea8f0a60b0d2
Hafiz Muneeb <muneeb@prismatech.agency>
2021-09-17 06:34:50 +0500

    Routes are revised

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'users/list', to: 'user#index'
+  #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #Singular Rersouce
+  #get 'user/list', to: 'user#index'
+  #Resource Routing
+  resources :user
 end
